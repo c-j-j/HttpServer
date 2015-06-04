fbc4050ba5e1440b66928424ca67dbd3f1b8d3ea
= <chrisjordan1987@gmail.com>
2015-06-04 21:14:23 +0100

    Adds not method allowed for put requests.

--- src/main/java/http/HTTPStatusCode.java
+++ src/main/java/http/HTTPStatusCode.java
@@ -3,7 +3,7 @@ package http;
 public enum HTTPStatusCode {
     OK(200, "OK"),
     FOUND(302, "Found"),
-    NOT_FOUND(404, "Not Found");
+    NOT_FOUND(404, "Not Found"), METHOD_NOT_ALLOWED(405, "Method Not Allowed");
 
     private int code;
     private String status;
--- src/test/java/http/response/PUTResponseResolverTest.java
+++ src/test/java/http/response/PUTResponseResolverTest.java
@@ -3,17 +3,39 @@ package http.response;
 import builders.RequestBuilder;
 import http.HTTPStatusCode;
 import http.Response;
+import org.apache.commons.io.FileUtils;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 import java.io.File;
+import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PUTResponseResolverTest {
 
+    @Rule
+    public TemporaryFolder temporaryFolder = new TemporaryFolder();
+    private File baseDir;
+
+    @Before
+    public void setUp() throws IOException {
+        baseDir = temporaryFolder.newFolder();
+    }
     @Test
     public void yields200Status() {
-        Response response = new PUTResponseResolver().getResponse(new File(""), new RequestBuilder().build());
+        Response response = new PUTResponseResolver().getResponse(baseDir, new RequestBuilder().withPath("/new_file").build());
         assertThat(response.getStatusCode()).isEqualTo(HTTPStatusCode.OK);
+    }
+
+    @Test
+    public void methodNotAllowedWhenFileExists() throws IOException {
+        File existentFile = new File(baseDir, "existentFile");
+        FileUtils.writeStringToFile(existentFile, "I already exist");
+
+        Response response = new PUTResponseResolver().getResponse(baseDir, new RequestBuilder().withPath("/existentFile").build());
+        assertThat(response.getStatusCode()).isEqualTo(HTTPStatusCode.METHOD_NOT_ALLOWED);
     }
 }
