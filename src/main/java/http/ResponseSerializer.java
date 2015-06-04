cdb77255543bb01c4ef298d66a08ad2394f34fda
= <chrisjordan1987@gmail.com>
2015-06-04 14:23:34 +0100

    refactor

--- src/main/java/http/ResponseSerializer.java
+++ src/main/java/http/ResponseSerializer.java
@@ -7,23 +7,30 @@ public class ResponseSerializer implements Serializer {
     @Override
     public String toPayload(Response response) {
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(headerLine(response));
+        stringBuilder.append(header(response));
         stringBuilder.append(location(response));
         stringBuilder.append(content(response));
         return stringBuilder.toString();
-    }
-
-    private String location(Response response) {
-        return StringUtils.isNotEmpty(response.getLocation()) ?
-                String.format("Location: %s\n", response.getLocation()) : "";
     }
 
     private String content(Response response) {
         return String.format("\n%s", response.getContents());
     }
 
-    private String headerLine(Response response) {
+    private String header(Response response) {
         HTTPStatusCode statusCode = response.getStatusCode();
+        StringBuilder headerBuilder = new StringBuilder();
+        headerBuilder.append(statusLine(statusCode));
+        headerBuilder.append(location(response));
+        return headerBuilder.toString();
+    }
+
+    private String statusLine(HTTPStatusCode statusCode) {
         return String.format("HTTP/1.1 %d %s\n", statusCode.getCode(), statusCode.getStatus());
+    }
+
+    private String location(Response response) {
+        return StringUtils.isNotEmpty(response.getLocation()) ?
+                String.format("Location: %s\n", response.getLocation()) : "";
     }
 }
