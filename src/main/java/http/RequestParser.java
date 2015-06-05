5a59edffc82e8dbef859308b8f8e16700f8002f5
= <chrisjordan1987@gmail.com>
2015-06-05 10:30:48 +0100

    Socket reading moved to separate class

--- src/main/java/http/RequestParser.java
+++ src/main/java/http/RequestParser.java
@@ -1,11 +1,5 @@
 package http;
 
-import org.apache.commons.lang.StringUtils;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.UncheckedIOException;
 import java.net.Socket;
 import java.util.function.Function;
 
@@ -13,24 +7,9 @@ public class RequestParser implements Function<Socket, Request> {
 
     @Override
     public Request apply(Socket socket) {
-        String requestPayload = getRequestPayload(socket);
+        String requestPayload = new SocketReader().getPayload(socket);
         System.out.println(requestPayload);
         return new Request(getAction(requestPayload), getPath(requestPayload));
-    }
-
-    private String getRequestPayload(Socket socket) {
-        try {
-            StringBuilder stringBuilder = new StringBuilder();
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-            String nextLine;
-            while (StringUtils.isNotEmpty(nextLine = bufferedReader.readLine())) {
-                stringBuilder.append(nextLine).append("\n");
-            }
-            return stringBuilder.toString();
-        } catch (IOException e) {
-            throw new UncheckedIOException(e);
-        }
     }
 
     private String getPath(String requestPayload) {
